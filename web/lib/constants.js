77bed832ad93e841bd63e3ede98828c2a4ad35ca
Avdhoot Patil <avdhoot.980@gmail.com>
2024-01-02 13:05:20 +0530

    Added homepage in web

---
+++ web/lib/constants.js
@@ -0,0 +1,14 @@
+export const routes = [
+  {
+    id: 1,
+    path: "/",
+    name: "Timesheet",
+    children: [],
+  },
+  {
+    id: 1,
+    path: "/register",
+    name: "Register",
+    children: [],
+  },
+];
